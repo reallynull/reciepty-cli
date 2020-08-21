e8ef12dde78eb0b960ca43e2d98b0719f0355d94
reallynull <67848586+reallynull@users.noreply.github.com>
2020-08-21 19:15:56 -0400

    Removed the function bit

--- reciepty.py
+++ reciepty.py
@@ -1,12 +1,11 @@
 import os
 import sys
 import json
-def test():
-    print('Welcome to receipty, a CLI receipt saving program with dozens of useful features to interact with your receipts!')
-    print('Would you like to either [1], save a receipt, or [2], check your receipt database, or [3], exit the program')
-    x = input()
-    if x == '3': 
-        sys.exit()
+print('Welcome to receipty, a CLI receipt saving program with dozens of useful features to interact with your receipts!')
+print('Would you like to either [1], save a receipt, or [2], check your receipt database, or [3], exit the program')
+x = input()
+if x == '3': #
+    sys.exit()
     if x == '[3]':
         sys.exit()
     if x == '1':
@@ -23,12 +22,19 @@ def test():
             item_one_price = input()
             print('Is that all you are saving to the receipt database?')
             receipt_answer = input()
+            dictionary = {
+                item_one_price: 'Price'
+            }
+            json_object = json.dumps(dictionary, indent=4)
+            with open("database.json", "w") as outfile:
+                outfile.write(json_object)
             if receipt_answer == 'yes':
                 sys.exit()
             if receipt_answer == 'yes':
                 sys.exit()
             else:
                 os.system('python reciepty.py')
+            # Data to be written
             if int(total_receipt_items) > 5:
                 print('Please enter the names and prices of your 5 or more items.')
                 print('What is the name of item one?')
@@ -50,9 +56,8 @@ def test():
         print('What is the total price of all items on your receipt?')
     if x == '2':
         print('The price for your last item is')
-        with open('C:/Users/username/Downloads/receipty/database.json') as jsonfile:
+        with open('C:/Users/snara/Downloads/receipty/database.json') as jsonfile:
             data = json.load(jsonfile)
-            print(data["item_one_price"])
+            print(data[item_one_price])
     if x == '[2]':
         print('Loading receipt database...')
-test()
